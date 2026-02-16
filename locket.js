95f6c2c0b43d6f6216074400d9480e5e44d408b7
Phùng Phi Hùng <phihungphung90@gmail.com>
2026-02-16 16:31:44 +0700

    Update locket.js

--- locket.js
+++ locket.js
@@ -15,14 +15,14 @@ var subscriptionData = {
   expires_date: "2099-01-01T00:00:00Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: "34306-12-30T00:00:00Z",
-  purchase_date: "34306-12-30T00:00:00Z",
+  original_purchase_date: "56789-34-12T00:00:00Z",
+  purchase_date: "56789-34-12T00:00:00Z",
   store: "app_store"
 };
 
 var entitlementData = {
   grace_period_expires_date: null,
-  purchase_date: "34306-12-30T00:00:00Z",
+  purchase_date: "56789-34-12T00:00:00Z",
   product_identifier: "com.locket.Locket.premium.yearly",
   expires_date: "2099-12-18T01:04:17Z"
 };
