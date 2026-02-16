88336e366bd12f7e6b1f1837c52c721b7cf52b1d
Phùng Phi Hùng <phihungphung90@gmail.com>
2026-02-16 10:27:01 +0700

    Update 100000910061982.js

--- 100000910061982.js
+++ 100000910061982.js
@@ -1,48 +1,45 @@
+// ========= ID ========= //
 const mapping = {
   '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
   'Locket': ['Gold']
 };
-
-var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
-var obj = JSON.parse($response.body);
+// =========   Phần cố định  ========= // 
+// =========  @duyvinh09 ========= // 
+var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
+  obj = JSON.parse($response.body);
 obj.Attention = "Made for 100000910061982 | By AntiDetectHungChannels";
-
-var subscriptionData = {
-  is_sandbox: false,
-  ownership_type: "PURCHASED",
-  billing_issues_detected_at: null,
-  period_type: "normal",
-  expires_date: "2099-01-01T00:00:00Z",
-  grace_period_expires_date: null,
-  unsubscribe_detected_at: null,
-  original_purchase_date: "34306-01-01T00:00:00Z",
-  purchase_date: "2006-07-03T00:00:00Z",
-  store: "app_store"
-};
-
-var entitlementData = {
-  grace_period_expires_date: null,
-  purchase_date: "2006-07-03T00:00:00Z",
-  product_identifier: "com.locket.Locket.premium.yearly",
-  expires_date: "2099-12-18T01:04:17Z"
-};
-
+var duyvinh09 = {
+      auto_resume_date: null,
+      display_name: "locket_1600_1y",
+      is_sandbox: true,
+      ownership_type: "PURCHASED",
+      billing_issues_detected_at: null,
+      management_url: "https://apps.apple.com/account/subscriptions",
+      period_type: "normal",
+      price: {
+          "amount": 399000.0,
+          "currency": "VND"
+      },
+      expires_date: "9999-01-09T10:10:14Z",
+      grace_period_expires_date: null,
+      refunded_at: null,
+      unsubscribe_detected_at: null,
+      original_purchase_date: "2005-01-09T10:10:15Z",
+      purchase_date: "2006-07-03T10:10:14Z",
+      store: "app_store",
+      store_transaction_id: "2000001108724193",
+  },
+  locketGold = {
+      grace_period_expires_date: null,
+      purchase_date: "2006-07-03T10:10:14Z",
+      product_identifier: "locket_1600_1y",
+      expires_date: "9999-01-09T10:10:14Z"
+  };
 const match = Object.keys(mapping).find(e => ua.includes(e));
-
 if (match) {
   let [e, s] = mapping[match];
-  
-  if (s) {
-    entitlementData.product_identifier = s;
-    obj.subscriber.subscriptions[s] = subscriptionData;
-  } else {
-    obj.subscriber.subscriptions["com.locket.Locket.premium.yearly"] = subscriptionData;
-  }
-  
-  obj.subscriber.entitlements[e] = entitlementData;
-} else {
-  obj.subscriber.subscriptions["com.locket.Locket.premium.yearly"] = subscriptionData;
-  obj.subscriber.entitlements.pro = entitlementData;
-}
-
-$done({body: JSON.stringify(obj)});
+  s ? (locketGold.product_identifier = s, obj.subscriber.subscriptions[s] = duyvinh09) : obj.subscriber.subscriptions["locket_1600_1y"] = duyvinh09, obj.subscriber.entitlements[e] = locketGold
+} else obj.subscriber.subscriptions["locket_1600_1y"] = duyvinh09, obj.subscriber.entitlements.pro = locketGold;
+$done({
+  body: JSON.stringify(obj)
+});
